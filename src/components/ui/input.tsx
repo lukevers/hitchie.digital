146dbb29f35c30eccf56da438b2597714a0c8ab0
Luke Evers <luke@lukevers.com>
2023-07-25 12:57:15 -0400

    Use correct form input size

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'flex h-10 w-full border text-rose-950 border-rose-900 bg-rose-100 px-3 py-2 text-sm ring-offset-rose-50 file:border-0 file:bg-transparent file:text-sm file:font-medium focus:outline-none focus:ring-1 focus:ring-rose-950 placeholder:text-rose-950/50 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+          'flex h-10 w-full border text-rose-950 border-rose-900 bg-rose-100 px-3 py-2 ring-offset-rose-50 file:border-0 file:bg-transparent file:text-sm file:font-medium focus:outline-none focus:ring-1 focus:ring-rose-950 placeholder:text-rose-950/50 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
           className
         )}
         ref={ref}
