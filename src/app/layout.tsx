92bd9b522ebc867bfdea332df0209861d94c28b9
Luke Evers <luke@lukevers.com>
2023-07-25 14:30:14 -0400

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,20 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import technologies from './(public)/technologies';
 import Axe from './axe';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Hitchie Digital',
+  title: 'Hitchie Digital | Software Engineering Agency',
   description: 'Hitchie Digital is a full service software engineering agency offering full-stack and infrastructure hands-on-coding and team coaching.',
+  applicationName: 'Hitchie Digital',
+  creator: 'Hitchie Digital',
+  keywords: ['Software', 'Engineering', 'Agency'].concat(technologies),
+  themeColor: '#ffe4e6',
+  colorScheme: 'only light',
+  robots: 'index, follow',
 };
 
 export default function RootLayout({
